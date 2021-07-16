dca43eb114dd55cba7fe1ab9ee6afcce0db874d9
JYao <jyao@prayinprayer.com>
2021-07-16 01:07:44 -0700

    final test deploy

--- prayinprayer/src/App.js
+++ prayinprayer/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi there
-          Edit <code>src/App.js</code> and save to reload.
+          Edit oh my....<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
